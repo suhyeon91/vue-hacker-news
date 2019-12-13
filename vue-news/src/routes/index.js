49e2b1a163dc706960f1c94c17aca078716eb2ea
suhyeon <suhyeon91.lee@gmail.com>
2019-12-13 17:57:29 +0900

    mixins

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+import AskView from '../views/AskView.vue';
 import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
 import createListView from '../views/CreateListView.js'
@@ -17,15 +20,18 @@ export const router = new VueRouter({
             //component: url 주소로 갔을 때 표시될 컴포넌트(페이지)
             path: '/news',
             name: 'news',
-            component: createListView('NewsView')
+            component: NewsView
+            // component: createListView('NewsView')
         }, {
             path: '/ask',
             name: 'ask',
-            component: createListView('AskView')
+            component: AskView
+            // component: createListView('AskView')
         }, {
             path: '/jobs',
             name: 'jobs',
-            component: createListView('JobsView')
+            component: JobsView
+            // component: createListView('JobsView')
         }, {
             path: '/item/:id',
             component: ItemView
--- vue-news/src/views/CreateListView.js
+++ vue-news/src/views/CreateListView.js
@@ -10,12 +10,12 @@ export default function createListView(name){
     created(){
       bus.$emit('start:spinner');
 
-    this.$store.dispatch('FETCH_LIST', this.$route.name)
-      .then(() => {
-        console.log('fetched');
-        bus.$emit('end:spinner');
-      })
-      .catch((error) => console.log(error));
+      this.$store.dispatch('FETCH_LIST', this.$route.name)
+        .then(() => {
+          console.log('fetched');
+          bus.$emit('end:spinner');
+        })
+        .catch((error) => console.log(error));
     },
     render(createElement){
         return createElement(ListView);
